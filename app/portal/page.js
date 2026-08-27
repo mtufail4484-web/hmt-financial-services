61c2f78538dca6d5d1e5e5ede82d5d62da47ced3
Muhammad Tufail <m.tufail4484@gmail.com>
2026-08-27 12:42:14 +0500

    Fix isAtLeastRole import in app/portal/page.js

--- app/portal/page.js
+++ app/portal/page.js
@@ -14,6 +14,7 @@ import {
 import {
   ROLES,
   getEffectiveRole,
+  isAtLeastRole,
   canManageStaff,
   canDownloadBackups,
   canManageLectures,
@@ -21,6 +22,7 @@ import {
   canReviewAssignments,
   canReplyQuestions,
   OWNER_UID,
+  OWNER_EMAIL,
 } from "../../lib/roles";
 import { generateSecret, verifyTOTP, getOtpAuthUri, generateBackupCodes, verifyBackupCode } from "../../lib/totp";
 import {
